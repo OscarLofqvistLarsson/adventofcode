7763230825a711cc0287cf3914bb0b107781b25d
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2025-12-01 23:29:18 +0100

    part2 added part1 since I just continued on it

--- 2025/day1.py
+++ 2025/day1.py
@@ -1,4 +1,4 @@
-def main():
+def part1():
 
     dial = list(range(100))
     current = 50
@@ -20,5 +20,40 @@ def main():
                     points += 1
     print(points)
 
+def part2():
+    current = 50
+    points = 0
+
+    with open("2025/inputday1.txt", "r") as input:
+
+        lines = input.readlines()
+        for line in lines:
+            dir = line[0]
+            steps = int(line[1:])
+            old = current
+
+            if dir == "R":
+                current = (old + steps)
+                rotations = current // 100
+                points += rotations
+
+                if old > 0 >= current:
+                    points += 1
+
+                current = current % 100
+
+            else:
+                current = (old - steps)
+                rotations = abs(current) // 100
+                points += rotations
+
+                if old > 0 >= current:
+                    points += 1
+
+                current = current % 100
+
+    print(points)
+
 if __name__ == "__main__":
-    main()
+    part1()
+    part2()
