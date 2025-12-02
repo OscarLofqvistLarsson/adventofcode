7380ec60ec215f9d9154e8892c4d0dfa4cce216b
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2025-12-02 16:13:42 +0100

    part 1 day 2

---
+++ 2025/day2.py
@@ -0,0 +1,24 @@
+def part1():
+    with open ("2025/inputday2.txt", "r") as input:
+
+        invalid = 0
+
+        for line in input:
+            lst = line.split(",")
+            parsed = [int(n) for _ in lst for n in _.split("-")]
+
+        while len(parsed) >= 2:
+            start, end = parsed[0], parsed[1]
+            for j in range(start , end + 1):
+                if str(j)[len(str(j))//2:] == str(j)[:len(str(j))//2]:
+                    invalid += j
+            parsed = parsed[2:]
+        print(invalid)
+
+
+
+
+
+if __name__ == "__main__":
+    part1()
+    #part2()
