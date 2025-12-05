5dff74176dd97c860c6ebf42dfc06f4b8a3df9cb
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2025-12-05 21:34:19 +0100

    day 5 part 1 range is num 1 op

--- 2025/day5.py
+++ 2025/day5.py
@@ -1,28 +1,25 @@
 def part1():
 
-    with open("2025/ex.txt") as input:
+    with open("2025/inputday5.txt") as input:
         ranges = []
         IDs = set()
         for line in input:
+            line = line.strip()
             if "-" in line:
-                ranges.append(line.strip())
+                ranges.append(tuple(map(int, line.split("-"))))
             if "-" not in line:
-                if line == "\n":
+                if line == "":
                     continue
                 else:
-                    IDs.add(int(line.strip()))
+                    IDs.add(int(line))
 
-        fresh = 0
-        counted = set()
-        for r in ranges:
-            start, end = map(int, r.split("-"))
-            for i in range(start, end + 1):
-                if i in IDs and i not in counted:
-                    fresh += 1
-                    counted.add(i)
+        fresh = set()
+        for start, end in ranges:
+            for i in IDs:
+                if start < i < end:
+                    fresh.add(i)
 
-
-        print(f"Amount of fresh ingrediens {fresh} \n")
+        print(f"Amount of fresh ingrediens {len(fresh)} \n")
 
 
 def part2():
